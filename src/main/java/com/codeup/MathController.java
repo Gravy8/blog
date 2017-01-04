32589931f5f45afbc9646abfab02dd1888c61b3f
Brad Colton <bradcolton11@yahoo.com>
2017-01-04 11:04:14 -0600

    Initial commit

---
+++ src/main/java/com/codeup/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String add(@PathVariable int numberOne, @PathVariable int numberTwo) {
+        return numberOne + " + " + numberTwo + " = " + (numberOne + numberTwo);
+    }
+
+    @GetMapping("/subtract/{numberOne}/from/{numberTwo}")
+    @ResponseBody
+    public String subtract(@PathVariable int numberOne, @PathVariable int numberTwo){
+        return numberTwo + " - " + numberOne + " = " + (numberTwo - numberOne);
+    }
+
+    @GetMapping("/multiply/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String multiply(@PathVariable int numberOne, @PathVariable int numberTwo){
+        return numberOne + " * " + numberTwo + " = " + (numberOne * numberTwo);
+    }
+
+    @GetMapping("/divide/{numberOne}/by/{numberTwo}")
+    @ResponseBody
+    public String divide(@PathVariable int numberOne, @PathVariable int numberTwo){
+        return numberOne + " / " + numberTwo + " = " + (numberOne / numberTwo);
+    }
+}
