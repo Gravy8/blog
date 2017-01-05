56336ddd1718de134e2ede8ccfe0aa0e386bfc09
Brad Colton <bradcolton11@yahoo.com>
2017-01-05 10:16:38 -0600

    completed dice roll exercise

---
+++ src/main/java/com/codeup/DiceRollController.java
@@ -0,0 +1,26 @@
+package com.codeup;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceRollController {
+
+    @GetMapping("/roll-dice")
+    public String roll() {
+        return "roll_dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String roll(@PathVariable int guess, Model model) {
+        int randomNumber = (int) Math.floor(Math.random() * 6) + 1;
+        boolean isCorrect = guess == randomNumber;
+        model.addAttribute("guess", guess);
+        model.addAttribute("roll", randomNumber);
+        model.addAttribute("isCorrect", isCorrect);
+        return "roll_dice";
+    }
+}
