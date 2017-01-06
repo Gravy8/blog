53c1f40c446602fa93a8dbc31ed6178e29298191
Brad Colton <bradcolton11@yahoo.com>
2017-01-06 12:07:46 -0600

    created standard dao factory

---
+++ src/main/java/com/codeup/dao/DaoFactory.java
@@ -0,0 +1,13 @@
+package com.codeup.dao;
+
+
+public class DaoFactory {
+    private static Posts postsDao;
+
+    public static Posts getPostsDao() {
+        if (postsDao == null) {
+            postsDao = new ListPosts();
+        }
+        return postsDao;
+    }
+}
