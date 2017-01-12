24bb0eae527f698aef22dd24d9476ad124f013aa
Brad Colton <bradcolton11@yahoo.com>
2017-01-12 14:32:31 -0600

    no longer using. Was used to access hibernateposts dao

--- src/main/java/com/codeup/dao/DaoFactory.java
+++ src/main/java/com/codeup/dao/DaoFactory.java
@@ -1,13 +1,21 @@
-package com.codeup.dao;
-
-
-public class DaoFactory {
-    private static Posts postsDao;
-
-    public static Posts getPostsDao() {
-        if (postsDao == null) {
-            postsDao = new ListPosts();
-        }
-        return postsDao;
-    }
-}
+//package com.codeup.dao;
+//
+//
+//import com.codeup.models.HibernateUtil;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//
+//public class DaoFactory {
+//
+//    private static SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//    private static Session session = sessionFactory.openSession();
+//
+//    private static Posts postsDao;
+//
+//    public static Posts getPostsDao() {
+//        if (postsDao == null) {
+//            postsDao = new HibernatePostsDao(session);
+//        }
+//        return postsDao;
+//    }
+//}
