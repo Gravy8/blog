90ac69a540608df7932bc8b929d17b77f95e7c40
Brad Colton <bradcolton11@yahoo.com>
2017-01-12 14:43:51 -0600

    created posts interface

--- src/main/java/com/codeup/dao/Posts.java
+++ src/main/java/com/codeup/dao/Posts.java
@@ -2,10 +2,19 @@ package com.codeup.dao;
 
 
 import com.codeup.models.Post;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface Posts {
-    List<Post> getAllPosts();
-    void savePost(Post post);
+public interface Posts extends CrudRepository<Post, Long> {
+
+    List<Post> findAll();
+    Post findById(int id);
+//    void savePost(Post post);
+
+//    List<Post> getAllPosts();
+//    void savePost(Post post);
+//    Post getPostById(int id);
+//    void updatePost(Post post);
+//    void deletePost(Post post);
 }
