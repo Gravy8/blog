59ef93dfdcdf893e4c3ea915ca09f77f352e2f3d
Brad Colton <bradcolton11@yahoo.com>
2017-01-12 14:30:20 -0600

    created controller for /login

---
+++ src/main/java/com/codeup/controllers/AuthenticationController.java
@@ -0,0 +1,12 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+    @GetMapping("/user/login")
+    public String showLoginForm() {
+        return "user/login";
+    }
+}
