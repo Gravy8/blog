72b910374b0068e88e49bb827a56641a41794007
Brad Colton <bradcolton11@yahoo.com>
2017-01-12 14:43:42 -0600

    created Post model

--- src/main/java/com/codeup/models/Post.java
+++ src/main/java/com/codeup/models/Post.java
@@ -1,33 +1,105 @@
 package com.codeup.models;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.Date;
 
+/**
+ * Created by Fer on 1/5/17.
+ */
+@Entity
+@Table(name="posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Title can't be empty.")
+    @Size(min = 3, message = "A title must be at least 3 characters.")
+    @Column(nullable = false)
     private String title;
+
+    @NotBlank(message = "Body can't be empty")
+    @Column(nullable = false, length = 2000)
     private String body;
 
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modify_date")
+    private Date modifyDate;
+
+    @ManyToOne
+    private User user;
+
+    public Post(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post() {
+
+    }
 
     public int getId() {
         return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getBody() {
-        return body;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getBody() {
+        return body;
+    }
+
     public void setBody(String body) {
         this.body = body;
+    }
+
+    @Override
+    public String toString(){
+        return "Title: "+ this.getTitle() + " Body: "+ this.getBody();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
